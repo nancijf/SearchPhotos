8d692c6f64e639b977fcc9b14a791825f96c642f
Nanci Frank <me@nancifrank.com>
2016-07-29 07:56:43 -0400

    Fixed singleimage VC to accurately  scale image

--- SearchPhotos/SingleImageViewController.swift
+++ SearchPhotos/SingleImageViewController.swift
@@ -20,15 +20,39 @@ class SingleImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let image = imageCache?.objectForKey(indexPath!)
-        imageView.image = image as? UIImage
+        let image = imageCache?.objectForKey(indexPath!) as? UIImage
+        
+//        if image!.size.width > image!.size.height {
+//            if image!.size.width > view.frame.size.width {
+//                scaledImage = scaleImage(image!, maxSize: view.frame.size.width)
+//            }
+//        } else {
+//            if image!.size.height > view.frame.size.height {
+//                scaledImage = scaleImage(image!, maxSize: view.frame.size.height)
+//            }
+//        }
 
-        // Do any additional setup after loading the view.
+        imageView.image = image
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+//    func scaleImage(photo: UIImage, maxSize: CGFloat) -> UIImage {
+//        
+//        let maxEdge = max(photo.size.height, photo.size.width)
+//        let scale = maxSize/maxEdge
+//        let scaledSize = CGSize(width: photo.size.width * scale, height: photo.size.height * scale)
+//        let rect = CGRect(origin: CGPointZero, size: scaledSize)
+//        
+//        UIGraphicsBeginImageContextWithOptions(scaledSize, false, 1.0)
+//        photo.drawInRect(rect)
+//        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        
+//        return newImage
+//    }
 
 }
